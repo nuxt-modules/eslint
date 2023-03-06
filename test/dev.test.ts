3437da0eae5f7b89849ab01da06924c26e0a7eba
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2023-03-06 15:18:28 -0300

    test: add simple test

---
+++ test/dev.test.ts
@@ -0,0 +1,15 @@
+import { describe, test, expect } from 'vitest'
+import { setup, useTestContext } from '@nuxt/test-utils'
+
+describe('dev', async () => {
+  await setup({
+    fixture: '../playground',
+    server: false,
+    dev: true
+  })
+
+  test('should added eslint plugin', () => {
+    const { nuxt } = useTestContext()
+    expect(nuxt?.hooks._hooks['vite:extendConfig']).toHaveLength(3)
+  })
+})
---
+++ test/prod.test.ts
@@ -0,0 +1,15 @@
+import { describe, test, expect } from 'vitest'
+import { setup, useTestContext } from '@nuxt/test-utils'
+
+describe('prod', async () => {
+  await setup({
+    fixture: '../playground',
+    server: false,
+    dev: false
+  })
+
+  test('should not added eslint plugin', () => {
+    const { nuxt } = useTestContext()
+    expect(nuxt?.hooks._hooks['vite:extendConfig']).toHaveLength(4)
+  })
+})
