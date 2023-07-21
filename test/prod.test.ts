681c664b28024de2b318478f2edafa9b0eceb241
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2023-07-21 14:16:55 -0300

    test: update

--- test/prod.test.ts
+++ test/prod.test.ts
@@ -10,6 +10,6 @@ describe('prod', async () => {
 
   test('should not added eslint plugin', () => {
     const { nuxt } = useTestContext()
-    expect(nuxt?.hooks._hooks['vite:extendConfig']).toHaveLength(5)
+    expect(nuxt?.hooks._hooks['vite:extendConfig']).toHaveLength(6)
   })
 })
